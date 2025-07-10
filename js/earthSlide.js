08082e70324b8227744145e67207a31b04da9062
Mac <467shin@gmail.com>
2025-07-11 01:37:25 +0900

    포폴은 노션이 좋겠어

--- js/earthSlide.js
+++ js/earthSlide.js
@@ -1,3 +1,4 @@
+/** 천지창조 */
 import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
 import { OrbitControls } from "https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js";
 
